4758d779fadbb8dbb3ee4ac83073048645241904
tongji4m3 <1254931237@qq.com>
2020-12-19 10:36:44 +0800

    JVM调优实验

--- src/main/java/tongji/util/thread/Main.java
+++ src/main/java/tongji/util/thread/Main.java
@@ -1,14 +1,15 @@
 package tongji.util.thread;
 
 
-import sun.misc.Unsafe;
-
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Executor;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class Main{
+    private static final int _1MB = 1024 * 1024;
+
     public static void main(String[] args) {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB]; //出现Minor GC
     }
 }
